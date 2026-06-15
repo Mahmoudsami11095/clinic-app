75f3e70e2883186950c5b3aba63f51d97f0a0492
Mahmoudsami11095 <37407727+Mahmoudsami11095@users.noreply.github.com>
2026-06-15 14:44:49 +0300

    chore: set production backend URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // TODO: Replace YOUR_BACKEND_APP_NAME with your actual Azure App Service URL from Step 2
-  apiUrl: 'https://YOUR_BACKEND_APP_NAME.azurewebsites.net/api'
+  apiUrl: 'https://clinic-api-123.azurewebsites.net/api'
 };
